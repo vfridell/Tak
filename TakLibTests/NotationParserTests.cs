bfeb15ba10a2620b1d3dcd88ad7d76aeb599835d
vfridell <gusknows@gmail.com>
2017-04-28 16:47:02 -0400

    stuff

---
+++ TakLibTests/NotationParserTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TakLibTests
+{
+    [TestClass]
+    public class NotationParserTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
