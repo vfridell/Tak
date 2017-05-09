abe436c87148fe106625a711bc4c6053fde3d335
vfridell <gusknows@gmail.com>
2017-05-09 16:59:32 -0400

    blah

---
+++ TakLib/BoardStats.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TakLib
+{
+    public class BoardStats
+    {
+        public Board Board { get; set; }
+        
+        public int BreathingSpaces { get; set; }
+
+    }
+
+    
+}
