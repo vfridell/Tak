107f3b71ad25bedabeb6b3d7f616b6244f930420
vfridell <gusknows@gmail.com>
2017-10-09 16:51:57 -0400

    Added checkboxes for graphs

--- TakWpfCustomControlLib/GameFactorsGraph.xaml.cs
+++ TakWpfCustomControlLib/GameFactorsGraph.xaml.cs
@@ -25,6 +25,8 @@ namespace TakWpfControls
     public partial class GameFactorsGraph : UserControl
     {
         private Game _game;
+        private ObservableCollection<AnalysisFactors> _factors;
+        private int _colorIndex = 0;
 
         public Game Game => _game;
 
@@ -34,38 +36,91 @@ namespace TakWpfControls
             _game = game;
             List<double> turns = new List<double>();
             MaximumRatioAnalyzer analyzer = new MaximumRatioAnalyzer(game.Boards[0].Size);
-            ObservableCollection<AnalysisFactors> factors = new ObservableCollection<AnalysisFactors>();
+            _factors = new ObservableCollection<AnalysisFactors>();
             int i = 0;
             foreach (Board board in Game.Boards)
             {
                 turns.Add(board.Round);
                 analyzer.Analyze(board);
-                factors.Add(analyzer.GetCurrentAnalysisFactors.Clone());
+                _factors.Add(analyzer.GetCurrentAnalysisFactors.Clone());
                 i++;
             }
 
-            // Create data sources:
-            EnumerableDataSource<AnalysisFactors> dataSource = new EnumerableDataSource<AnalysisFactors>(factors);
-            dataSource.SetXMapping(f => f["roundNumber"].Value);
-            dataSource.SetYMapping(f => f["flatScore"].CalculatedResult);
-            Chart.AddLineGraph(dataSource, Colors.Red, 2, "Flat Score");
-            EnumerableDataSource<AnalysisFactors> dataSource2 = new EnumerableDataSource<AnalysisFactors>(factors);
-            dataSource2.SetXMapping(f => f["roundNumber"].Value);
-            dataSource2.SetYMapping(f => f["stacksAdvantageDiff"].CalculatedResult);
-            Chart.AddLineGraph(dataSource2, Colors.Blue, 2, "Stacks Advantage");
-
-            //var y2DataSource = factors.Select(f => f.Values.FirstOrDefault(v => v.Name == "stacksAdvantageDiff")).Select(f => f.CalculatedResult).AsYDataSource();
-            //var y3DataSource = factors.Select(f => f.Values.FirstOrDefault(v => v.Name == "longestSubGraphDiff")).Select(f => f.CalculatedResult).AsYDataSource();
-            //var y4DataSource = factors.Select(f => f.Values.FirstOrDefault(v => v.Name == "averageSubGraphDiff")).Select(f => f.CalculatedResult).AsYDataSource();
-
-            //var factory = new EnumerableDataSourceFactory();
-            //var blah = factory.TryBuild(factors);
-
-            // adding graph to plotter
+            i = 0;
+            foreach (string featureIndex in _factors[0].Keys)
+            {
+                // Create data sources:
+                AddFeatureGraph(featureIndex, GetColor());
+                CheckBox newCheckBox = new CheckBox() { IsChecked = true, Content = FeatureIndexToName(featureIndex), Name = featureIndex};
+                RadioStackPanel.Children.Add(newCheckBox);
+                newCheckBox.Checked += (sender, args) =>
+                {
+                    AddFeatureGraph(newCheckBox.Name, GetColor());
+                };
+                newCheckBox.Unchecked +=
+                    (sender, args) =>
+                    {
+                        Chart.Children.Where(e => e is LineGraph)
+                            .FirstOrDefault(l => l.ToString() == FeatureIndexToName(featureIndex))?.Remove();
+                    };
+                i++;
+            }
 
             // Force evertyhing plotted to be visible
             Chart.FitToView();
         }
+
+        public void AddFeatureGraph(string featureIndex, Color color)
+        {
+            EnumerableDataSource<AnalysisFactors> dataSource = new EnumerableDataSource<AnalysisFactors>(_factors);
+            dataSource.SetXMapping(f => f["roundNumber"].Value);
+            dataSource.SetYMapping(f => f[featureIndex].CalculatedResult);
+            Chart.AddLineGraph(dataSource, color, 2, FeatureIndexToName(featureIndex));
+        }
+
+        public static string FeatureIndexToName(string featureIndex)
+        {
+            int i = 0;
+            string name = "";
+            foreach (char c in featureIndex)
+            {
+                char newC = c;
+                if (i == 0) newC = Char.ToUpper(c);
+                if (Char.IsUpper(newC) && i > 0)
+                {
+                    name = name + ' ';
+                }
+                name = name + newC;
+                i++;
+            }
+            name = name.Replace("Diff", "").Trim();
+            return name;
+        }
+
+        private Color GetColor()
+        {
+            _colorIndex++;
+            return _colors[_colorIndex % 16];
+        }
+        private static Dictionary<int, Color> _colors = new Dictionary<int, Color>()
+        {
+            {0,Colors.Blue},
+            {1,Colors.Red},
+            {2,Colors.Green},
+            {3,Colors.Orange},
+            {4,Colors.Black},
+            {5,Colors.DeepPink},
+            {6,Colors.DarkSlateGray},
+            {7,Colors.Tomato},
+            {8,Colors.Lime},
+            {9,Colors.Chocolate},
+            {10,Colors.Brown},
+            {11,Colors.CadetBlue},
+            {12,Colors.BlueViolet},
+            {13,Colors.GreenYellow},
+            {14,Colors.Firebrick},
+            {15,Colors.DarkSalmon},
+        };
     }
 
 }
